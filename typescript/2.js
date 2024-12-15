de2b41c26326280e8e817d5cc3455a45f654ec22
saeedsarkarati <saeedsarkarati@gmail.com>
2024-12-15 10:41:37 -0500

    یکشنبه - ۲۵ آذر ۱۴۰۳

---
+++ typescript/2.js
@@ -0,0 +1,5 @@
+var f = 1; // type string
+//firstName = 33; // attempts to re-assign the value to a different type
+console.log(f);
+f = "44";
+console.log(f);
---
+++ typescript/2.ts
@@ -0,0 +1,5 @@
+let f:any =1 // type string
+//firstName = 33; // attempts to re-assign the value to a different type
+console.log(f);
+f = "44"
+console.log(f);
